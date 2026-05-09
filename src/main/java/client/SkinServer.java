d38017a8a59e4b882036e2f5019b2ef589907a96
Aneesh <aneeshkatgal@gmain.com>
2026-05-09 14:28:53 +0530

    init commit

--- src/main/java/client/SkinServer.java
+++ src/main/java/client/SkinServer.java
@@ -5,6 +5,7 @@ import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import org.json.JSONObject;
 
+import javax.print.attribute.standard.MediaSize;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -15,11 +16,116 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SkinServer {
     static java.util.Map<String, String> skinMap = new java.util.HashMap<>();
 
     static Authenticator authenticator = new Authenticator();
+
+    static JSONObject pendingSessions = new JSONObject();
+
+    public static void updateSkinMap(String uuid, String fileName){
+        try {
+            Path path = Path.of("minecraft", "skins", "map.json");
+
+            JSONObject object;
+
+            if (Files.exists(path)){
+                String json = Files.readString(path);
+                object = new JSONObject(json);
+            } else {
+                object = new JSONObject();
+            }
+
+            object.put(uuid, fileName);
+
+            Files.writeString(path, object.toString(4));
+
+            skinMap.put(uuid, fileName);
+
+            System.out.println("updated");
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String fetchJson(String urlStr){
+        try {
+            URL url = new URL(urlStr);
+            InputStream is = url.openStream();
+            return new String(is.readAllBytes(), StandardCharsets.UTF_8);
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String getSkinUrl(String uuid){
+        String jsonStr = fetchJson("https://raw.githubusercontent.com/Omkar-Aneesh/SkinSystem/refs/heads/main/manifest.json");
+
+        JSONObject obj = new JSONObject(jsonStr);
+        String skinUrl = obj.getString(uuid);
+
+//            System.out.println("url:" + skinUrl);
+
+        return skinUrl;
+    }
+
+    public static void downloadFile(String fileUrl, String savePath){
+        try (InputStream in = new URL(fileUrl).openStream()){
+            Files.copy(in, Paths.get(savePath), StandardCopyOption.REPLACE_EXISTING);
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String generateProfileResponse(String uuid){
+        if (!skinMap.containsKey(uuid)) {
+            try {
+                String url = getSkinUrl(uuid);
+                downloadFile(url, "minecraft/skins/" + uuid + ".png");
+                updateSkinMap(uuid, uuid + ".png");
+            } catch (Exception e) {
+                System.err.println("Async download failed: " + e.getMessage());
+            }
+        }
+
+        String fileName = skinMap.getOrDefault(uuid, "steve.png");
+        String skinUrl = "http://localhost:8080/skins/" + fileName;
+
+        String username = authenticator.authenticate(uuid);
+
+        String textureJson = "{"
+                + "\"timestamp\":" + System.currentTimeMillis() + ","
+                + "\"profileId\":\"" + uuid + "\","
+                + "\"profileName\":\"" + username + "\","
+                + "\"signatureRequired\":false,"
+                + "\"textures\":{"
+                + "\"SKIN\":{"
+                + "\"url\":\"" + skinUrl + "\""
+                + "}"
+                + "}"
+                + "}";
+
+        System.out.println("TEXTURE JSON: " + textureJson);
+
+        String encoded = java.util.Base64.getEncoder().encodeToString(textureJson.getBytes(StandardCharsets.UTF_8));
+
+        String response = "{"
+                + "\"id\":\"" + uuid + "\","
+                + "\"name\":\"" + username + "\","
+                + "\"properties\":[{"
+                + "\"name\":\"textures\","
+                + "\"value\":\"" + encoded + "\""
+                + "}]"
+                + "}";
+
+        System.out.println("Skin URL: " + skinUrl);
+        System.out.println("Encoded length: " + encoded.length());
+
+        return response;
+    }
 
     static void loadSkinMap() {
         try {
@@ -53,7 +159,7 @@ public class SkinServer {
             server.setExecutor(java.util.concurrent.Executors.newCachedThreadPool());
             server.start();
 
-//            System.out.println("started");
+            System.out.println("started");
         } catch (Exception e){
             throw new RuntimeException(e);
         }
@@ -71,7 +177,7 @@ public class SkinServer {
             try (httpExchange) {
 
                 String path = httpExchange.getRequestURI().getPath();
-//                System.out.println(httpExchange.getRequestURI());
+                System.out.println(httpExchange.getRequestURI());
 
                 if (path.contains("/session/minecraft/profile/")) {
                     new ProfileHandler().handle(httpExchange);
@@ -80,6 +186,11 @@ public class SkinServer {
 
                 if (path.startsWith("/skins/")) {
                     new SkinHandler().handle(httpExchange);
+                    return;
+                }
+
+                if (path.contains("/session/minecraft/join") || path.contains("/session/minecraft/hasJoined")){
+                    new SessionHandler().handle(httpExchange);
                     return;
                 }
 
@@ -93,7 +204,11 @@ public class SkinServer {
                         + "    \"register\": \"http://localhost:8080/\""
                         + "  }"
                         + "},"
-                        + "\"skinDomains\": [\"localhost\"]"
+                        + "\"skinDomains\": [\"localhost\", \"127.0.0.1\"],"
+                        + "\"features\": {"
+                        + "  \"no_unshadowed_uuid\": true,"
+                        + "  \"unrestricted_texture_whitelist\": true"
+                        + "}"
                         + "}";
 
                 httpExchange.getResponseHeaders().add("Content-Type", "application/json");
@@ -127,56 +242,9 @@ public class SkinServer {
                 String[] parts = path.split("/");
                 String uuid = parts[parts.length - 1];
 
-//                System.out.println("UUID request: " + uuid);
+                System.out.println("UUID request: " + uuid);
 
-                if (!skinMap.containsKey(uuid)) {
-                    new Thread(() -> {
-                        try {
-                            String url = getSkinUrl(uuid);
-                            downloadFile(url, "minecraft/skins/" + uuid + ".png");
-                            updateSkinMap(uuid, uuid + ".png");
-                        } catch (Exception e) {
-                            System.err.println("Async download failed: " + e.getMessage());
-                        }
-                    }).start();
-
-                    httpExchange.sendResponseHeaders(204, -1);
-                    httpExchange.close();
-                    return;
-                }
-
-                String fileName = skinMap.getOrDefault(uuid, "steve.png");
-                String skinUrl = "http://localhost:8080/skins/" + fileName;
-
-                String username = authenticator.authenticate(uuid);
-
-                String textureJson = "{"
-                        + "\"timestamp\":" + System.currentTimeMillis() + ","
-                        + "\"profileId\":\"" + uuid + "\","
-                        + "\"profileName\":\"" + username + "\","
-                        + "\"signatureRequired\":false,"
-                        + "\"textures\":{"
-                        + "\"SKIN\":{"
-                        + "\"url\":\"" + skinUrl + "\""
-                        + "}"
-                        + "}"
-                        + "}";
-
-//                System.out.println("TEXTURE JSON: " + textureJson);
-
-                String encoded = java.util.Base64.getEncoder().encodeToString(textureJson.getBytes(StandardCharsets.UTF_8));
-
-                String response = "{"
-                        + "\"id\":\"" + uuid + "\","
-                        + "\"name\":\"" + username + "\","
-                        + "\"properties\":[{"
-                        + "\"name\":\"textures\","
-                        + "\"value\":\"" + encoded + "\""
-                        + "}]"
-                        + "}";
-
-//                System.out.println("Skin URL: " + skinUrl);
-//                System.out.println("Encoded length: " + encoded.length());
+                String response = SkinServer.generateProfileResponse(uuid);
 
                 httpExchange.getResponseHeaders().add("Content-Type", "application/json");
                 httpExchange.sendResponseHeaders(200, response.getBytes().length);
@@ -190,59 +258,56 @@ public class SkinServer {
                 httpExchange.sendResponseHeaders(500, -1);
             }
         }
+    }
 
-        public void updateSkinMap(String uuid, String fileName){
-            try {
-                Path path = Path.of("minecraft", "skins", "map.json");
+    static class SessionHandler implements HttpHandler {
 
-                JSONObject object;
+        @Override
+        public void handle(HttpExchange httpExchange) throws IOException {
+            String path = httpExchange.getRequestURI().getPath();
 
-                if (Files.exists(path)){
-                    String json = Files.readString(path);
-                    object = new JSONObject(json);
+            if (path.endsWith("/join")){
+                InputStream is = httpExchange.getRequestBody();
+                String body = new String(is.readAllBytes(), StandardCharsets.UTF_8);
+                JSONObject json = new JSONObject(body);
+
+                String uuid = json.getString("selectedProfile");
+                String serverId = json.getString("serverId");
+
+                pendingSessions.put(serverId, uuid);
+
+                httpExchange.sendResponseHeaders(204, -1);
+            } else if (path.endsWith("/hasJoined")) {
+                String query = httpExchange.getRequestURI().getQuery();
+                Map<String, String> params = parseQuery(query);
+
+                String serverId = params.get("serverId");
+                String uuid = pendingSessions.get(serverId).toString();
+
+                if (uuid != null){
+                    String response = SkinServer.generateProfileResponse(uuid);
+
+                    httpExchange.getResponseHeaders().add("Content-Type", "application/json");
+                    byte[] respBytes = response.getBytes(StandardCharsets.UTF_8);
+                    httpExchange.sendResponseHeaders(200, respBytes.length);
+                    httpExchange.getResponseBody().write(respBytes);
                 } else {
-                    object = new JSONObject();
+                    httpExchange.sendResponseHeaders(204, -1);
                 }
-
-                object.put(uuid, fileName);
-
-                Files.writeString(path, object.toString(4));
-
-                skinMap.put(uuid, fileName);
-
-//                System.out.println("updated");
-            } catch (Exception e){
-                throw new RuntimeException(e);
             }
+            httpExchange.close();
         }
 
-        public String fetchJson(String urlStr){
-            try {
-                URL url = new URL(urlStr);
-                InputStream is = url.openStream();
-                return new String(is.readAllBytes(), StandardCharsets.UTF_8);
-            } catch (Exception e){
-                throw new RuntimeException(e);
+        private Map<String, String> parseQuery(String query){
+            Map<String, String> result = new HashMap<>();
+            for (String param: query.split("&")){
+                String[] entry = param.split("=");
+                if (entry.length > 1){
+                    result.put(entry[0], entry[1]);
+                }
             }
-        }
 
-        public String getSkinUrl(String uuid){
-            String jsonStr = fetchJson("https://raw.githubusercontent.com/Omkar-Aneesh/SkinSystem/refs/heads/main/manifest.json");
-
-            JSONObject obj = new JSONObject(jsonStr);
-            String skinUrl = obj.getString(uuid);
-
-//            System.out.println("url:" + skinUrl);
-
-            return skinUrl;
-        }
-
-        public void downloadFile(String fileUrl, String savePath){
-            try (InputStream in = new URL(fileUrl).openStream()){
-                Files.copy(in, Paths.get(savePath), StandardCopyOption.REPLACE_EXISTING);
-            } catch (Exception e){
-                throw new RuntimeException(e);
-            }
+            return result;
         }
     }
 
